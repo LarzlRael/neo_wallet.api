2be0cb1d5dab1349ec78fe41a16c186e56251534
Reynaldo GT <larz.dev@gmail.com>
2022-08-28 22:42:02 -0400

    Changing routes imports

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,62 @@
-import express from 'express';
-const app = express();
-import morgan from 'morgan';
-require('dotenv').config();
-import path from 'path';
-import expressHandleBars from 'express-handlebars';
-import passport from 'passport';
-import passportMiddleware from './middlewares/passport';
-
-import authRoutes from './routes/auth';
-import walletRoutes from './routes/wallet';
-import transactionsRoutes from './routes/transaction';
-import qrRoutes from './routes/qr';
-import mailRoutes from './routes/mail';
+import express from 'express'
+const app = express()
+import morgan from 'morgan'
+require('dotenv').config()
+import path from 'path'
+import expressHandleBars from 'express-handlebars'
+import passport from 'passport'
+import passportMiddleware from './middlewares/passport'
 
 //DB config
-require('./database/databaseConfig').dbConnection();
+require('./database/databaseConfig').dbConnection()
 
-const port = process.env.PORT;
+const port = process.env.PORT
 //Node server
 
-const server = require('http').createServer(app);
-export const io = require('socket.io')(server);
-import './sockets/socket';
+const server = require('http').createServer(app)
+export const io = require('socket.io')(server)
+import './sockets/socket'
+import {
+  authRoutes,
+  mailRoutes,
+  qrRoutes,
+  transactionsRoutes,
+  walletRoutes,
+} from './routes'
 
-app.enable('trust proxy');
-app.set('views', path.join(__dirname, 'views'));
+app.enable('trust proxy')
+app.set('views', path.join(__dirname, 'views'))
 /* app.engine('.hbs', expressHandleBars({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs'
 })); */
-app.set('view engine', '.hbs');
+app.set('view engine', '.hbs')
 
 //Lectura y parse de BODY
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(morgan('dev'))
-app.use(passport.initialize());
-passport.use(passportMiddleware);
+app.use(passport.initialize())
+passport.use(passportMiddleware)
 
 //Index server index
-const publicPath = path.resolve(__dirname, '../public');
-app.use(express.static(publicPath));
+const publicPath = path.resolve(__dirname, '../public')
+app.use(express.static(publicPath))
 
 //routes
 
-app.use('/auth', authRoutes);
-app.use('/wallet', walletRoutes);
-app.use('/transactions', transactionsRoutes);
-app.use('/qr', qrRoutes);
-app.use('/sendmail', mailRoutes);
-
-
+app.use('/auth', authRoutes)
+app.use('/wallet', walletRoutes)
+app.use('/transactions', transactionsRoutes)
+app.use('/qr', qrRoutes)
+app.use('/sendmail', mailRoutes)
 
 server.listen(port, () => {
-    console.log(`Server on port ${port}`);
-});
-
+  console.log(`Server on port ${port}`)
+})
 
 //TODO add uploads file to image profile
 //TODO add google signing
 //TODO ?report by month
-
-
-
---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import authRoutes from './auth'
+import walletRoutes from './wallet'
+import transactionsRoutes from './transaction'
+import qrRoutes from './qr'
+import mailRoutes from './mail'
+
+export { authRoutes, walletRoutes, transactionsRoutes, qrRoutes, mailRoutes }
